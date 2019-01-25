4c03123d620f8fa1b42e4f1811cc98af4159d157
Maga Alkhasov <maga@alkhasov.com>
2019-01-25 22:41:14 +0400

    done next 3 mini-challenges

--- javascript/11-hexlet-challages/app.js
+++ javascript/11-hexlet-challages/app.js
@@ -81,6 +81,7 @@ console.log(`#5 reverse integer -1235`, reverseInt(-1235));
 
 const reverseStr = str => {
   return str.split('').reduce((acc, curr) => curr + acc);
+  //or reverse()
 };
 
 console.log(`#6 reverse string`, reverseStr('reverse string'));
@@ -119,8 +120,35 @@ const sumSquareDifference = n => {
 
 console.log(`#8 sum square diff, 10`, sumSquareDifference(15));
 
-//diff angle
+//minutes to hour:minutes https://ru.hexlet.io/challenges/programming_basics_time
 
+const formattedTime = mins => {
+  let hours = Math.floor(mins / 60);
+  let minutes = mins % 60;
+  if (hours < 10) hours = '0' + hours;
+  if (hours >= 24) hours = hours % 24;
+  if (minutes < 10) minutes = '0' + minutes;
+  return `${hours}:${minutes}`;
+};
+
+//console.log(`foormatted time 12:34`, formattedTime(754));
+console.log(`#9 foormatted time 1:00`, formattedTime(1500));
+
+//ackermann
+
+const ackermann = (m, n) => {
+  if (m === 0 && n >= 0) return n + 1;
+  if (m > 0 && n === 0) return ackermann(m - 1, 1);
+  if (m > 0 && n > 0) return ackermann(m - 1, ackermann(m, n - 1));
+  if (m < 0 || n < 0) return 0;
+};
+
+/* console.log(`(0, -1)`, ackermann(0, -1));
+console.log(`(0, 0)`, ackermann(0, 0));
+console.log(`(1, 1)`, ackermann(1, 1)); */
+console.log(`#10 ackerman(3, 7)`, ackermann(3, 7));
+
+//diff angle https://ru.hexlet.io/challenges/intro_to_programming_angle_difference
 const diff = (angleA, angleB, print) => {
   let preDiff;
   let actualDiff;
@@ -135,7 +163,10 @@ const diff = (angleA, angleB, print) => {
     actualDiff = preDiff;
   }
   print && console.log(`diff`, actualDiff);
+  return actualDiff;
 };
+
+console.log(`#11 min angle diff(45, 290)`, diff(45, 290));
 
 /*
 diff(0, 45, 1);
@@ -151,3 +182,19 @@ console.log(`test`, 115);
 diff(45, 190, 1);
 console.log(`test`, 145);
 */
+
+//perfect numbers https://ru.hexlet.io/challenges/intro_to_programming_perfect_numbers
+
+const isPerfect = num => {
+  if (num % 2 !== 0) return false; //because currently no found any odd perfect number
+
+  let delimeters = [];
+  for (let i = 1; i <= num / 2; i++) {
+    if (num % i === 0) delimeters.push(i);
+    //here maybe optimization like i *= i - 1
+  }
+  if (num === delimeters.reduce((acc, curr) => acc + curr)) return true;
+  return false;
+};
+
+console.log(`#12 isPerfect(8128)`, isPerfect(8128));
